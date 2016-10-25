3d33925a5d35d62987fb1ff79b3382ad039e1908
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2016-10-26 01:13:29 +0200

    Added start, end, junk-allowed parameters to parse in src/protocol.lisp
    * src/protocol.lisp (parse): added start, end and junk-allowed keyword
      parameters; adapted documentation string accordingly
      (define-condition-translating-method parse): accept start, end and
      junk-allowed keyword parameters
      (parse t t): likewise
      (parse string t): likewise
      (parse source t): likewise
      (parse pathname t): likewise
    * test/protocol.lisp (header): updated copyright
      (test parse.smoke): added cases for new `parse' keyword parameters

--- src/protocol.lisp
+++ src/protocol.lisp
@@ -8,16 +8,24 @@
 
 ;;; Parser protocol
 
-(defgeneric parse (source builder)
+(defgeneric parse (source builder &key start end junk-allowed)
   (:documentation
    "Parse the content of SOURCE as \"ini-like\" configuration options,
     construct a parse result using BUILDER and return it.
+
+    START and END can be used to restrict parsing to a sub-sequence of
+    SOURCE.
+
+    JUNK-ALLOWED controls whether an error is signaled when a
+    successful parse does not consume the entire input in SOURCE (or
+    the sub-sequence delimited by START and END).
 
     Signal a `ini-parse-error' when errors are encountered."))
 
 ;; Default behavior
 
-(define-condition-translating-method parse ((source t) (builder t))
+(define-condition-translating-method parse ((source t) (builder t)
+                                            &key start end junk-allowed)
   ;; When an `esrap-error' is signaled, the innermost `parse' method
   ;; is specialized on `string'. It is therefore OK to assign SOURCE
   ;; to the source slot of the condition.
@@ -29,18 +37,26 @@
   ((error ini-parse-error)
    :source source))
 
-(defmethod parse ((source t) (builder t))
+(defmethod parse ((source t) (builder t)
+                  &key start end junk-allowed)
+  (declare (ignore start end junk-allowed))
   (error "~@<Cannot parse source ~A with builder ~A.~@:>"
          source builder))
 
-(defmethod parse ((source string) (builder t))
+(defmethod parse ((source string) (builder t)
+                  &key (start 0) end junk-allowed)
   (architecture.builder-protocol:with-builder (builder)
     (esrap:parse 'ini source
+                 :start start :end end :junk-allowed junk-allowed
                  #+esrap.grammar-class :grammar
                  #+esrap.grammar-class '#:parser.ini)))
 
-(defmethod parse ((source stream) (builder t))
-  (parse (read-stream-content-into-string source) builder))
+(defmethod parse ((source stream) (builder t)
+                  &key (start 0) end junk-allowed)
+  (parse (read-stream-content-into-string source) builder
+         :start start :end end :junk-allowed junk-allowed))
 
-(defmethod parse ((source pathname) (builder t))
-  (parse (read-file-into-string source) builder))
+(defmethod parse ((source pathname) (builder t)
+                  &key (start 0) end junk-allowed)
+  (parse (read-file-into-string source) builder
+         :start start :end end :junk-allowed junk-allowed))
--- test/protocol.lisp
+++ test/protocol.lisp
@@ -1,6 +1,6 @@
 ;;;; protocol.lisp --- Unit tests for the protocol of the parser.ini system.
 ;;;;
-;;;; Copyright (C) 2013 Jan Moringen
+;;;; Copyright (C) 2013, 2016 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
@@ -12,12 +12,31 @@
   "Smoke test for the generic function `parse'."
 
   (mapc
-   (lambda+ ((input builder expected))
-     (let+ (((&flet do-it () (parse input builder))))
+   (lambda+ ((input builder args expected))
+     (let+ (((&flet do-it () (apply #'parse input builder args))))
        (case expected
          (ini-parse-error (signals ini-parse-error (do-it)))
          (t               (is (equal expected (do-it)))))))
 
-   `((:no-such-method   list             ini-parse-error)
-     (,#P"no-such-file" list             ini-parse-error)
-     ("foo = bar"       :no-such-builder ini-parse-error))))
+   `(;; Some invalid cases
+     (:no-such-method   list             ()         ini-parse-error)
+     (,#P"no-such-file" list             ()         ini-parse-error)
+     ("foo = bar"       :no-such-builder ()         ini-parse-error)
+
+     ;; These are valid
+     ("foo = bar"       list             ()
+      ((:section
+        (:section-option (((:option
+                            ()
+                            :name ("foo") :value "bar" :bounds (0 . 9)))))
+        :name nil)))
+
+     ("= foo = bar"     list             (:start 2)
+      ((:section
+        (:section-option (((:option
+                            ()
+                            :name ("foo") :value "bar" :bounds (2 . 11)))))
+        :name nil)))
+
+     ("= "              list             (:junk-allowed t)
+      ()))))
